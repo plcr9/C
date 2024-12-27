3389ff85c4314ff82a57d56129509abed0db8aae
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-27 00:21:10 +0000

    Update output.c

--- output.c
+++ output.c
@@ -1,1 +1,18 @@
-
+int main() {
+  printf
+  (
+    "2 Cups: All Purpose Flour\n"
+  )
+  ;
+  printf
+  (
+    "1 Cups: Unsalted Butter\t(Room Temperature)"
+  )
+  ;
+  printf
+  (
+    "\n2/3 Cups: Granulated Sugar"
+  )
+  ;
+}
+    
