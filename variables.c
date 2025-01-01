488a6be906d17d6f48600148876aa9adfae2c86f
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-01 14:52:28 +0000

    Update variables.c

--- variables.c
+++ variables.c
@@ -1,1 +1,13 @@
+int main() {
+  int studentRank;
+  double studentFeeTotal;
+  clear studentGradeLetter;
 
+studentRank = 1;
+studentFeeTotal = 100.56;
+studentGradeLetter = "A";
+
+printf("Student's Rank in class: %dst\n", studentRank);
+printf("Student's Total Fees: $%3.2f\n", studentFeeTotal);
+printf("Student's Grade: %c\n", studentGradeLetter);
+}
