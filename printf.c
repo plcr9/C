d23f0c7b59530eb65f373483995300581b83139d
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-02 08:30:18 +0000

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,1 +1,7 @@
+int main() {
 
+  int ageLearnedToRide = 5;
+
+  printf("I was %d years old when I learned to ride a bike.\n", ageLearnedToRide);
+  printf("I hope I still remember how to ride.");
+}
