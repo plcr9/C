77d74246b2861f00b34a2f8cda316a26e8c9180a
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-03 08:39:27 +0000

    Update variables_initialisation.c

--- variables_initialisation.c
+++ variables_initialisation.c
@@ -1,1 +1,12 @@
+int main() {
 
+  int numOfBooks = 2;
+  char favLetter = 'x';
+  char favDigit = '7';
+  double costOfCandyBar = 1.05;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your Favorite Letter is: %c\n", favLetter);
+  printf("Your Favorite Digit is: %c\n", favDigit);
+  printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
