1ecfc376a7076df4a7f31243bd372833c60cb86e
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-07 00:15:21 +0000

    Update updating_values.c

--- updating_values.c
+++ updating_values.c
@@ -1,1 +1,14 @@
+int main() {
+  char bookVersionReview = 'A';
+  char movieVersionReview = 'B';
+  double ticketPrice = 10.25;
+  double bookPrice = 19.99;
 
+  movieVersionReview = 'C';
+
+  ticketPrice = bookPrice;
+
+  printf("The book version has a review store of %c and costs $%.2f\n", bookVersionReview, bookPrice);
+
+  printf("The movie version has a review score of %c and costs $%.2f\n", movieVersionReview, ticketPrice);
+}
