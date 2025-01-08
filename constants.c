7a2f912fd3ef5fdcf510ab5217a166b0656b5fc6
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-08 08:08:16 +0000

    Update constants.c

--- constants.c
+++ constants.c
@@ -1,1 +1,7 @@
+int main() {
+  const double SPEEDOFLIGHT = 1.86;
+  int timeTravelInSeconds = 30;
 
+  printf("Light would travel %.2f miles in %d seconds\n", SPEEDOFLIGHT * 100000 * timeTravelInSeconds, timeTravelInSeconds);
+
+}
