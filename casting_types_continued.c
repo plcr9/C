b8f3cb3af2e7944d5cfd2ac5146c5885228f4739
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-10 07:08:38 +0000

    Update casting_types_continued.c

--- casting_types_continued.c
+++ casting_types_continued.c
@@ -1,1 +1,12 @@
+int main()
 
+  char targetChar;
+  int sourceInt = 99;
+  double sourceDouble = 55.67;
+
+  targetChar = (char)sourceInt;
+  targetChar = sourceDouble;
+
+  printf("source int %d, source double, %.2f, target %c\n", sourceInt, sourceDouble, targetChar);
+
+}
