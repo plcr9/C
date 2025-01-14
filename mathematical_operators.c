10e67bba4baae2a74c63a1675302b934d02a5c46
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-14 09:16:26 +0000

    Update mathematical_operators.c

--- mathematical_operators.c
+++ mathematical_operators.c
@@ -1,1 +1,12 @@
+int main() {
 
+  int booksSold = 100;
+  double bookCost = 9.99;
+  double totalSalesValue;
+
+  booksSold = booksSold + 200;
+  totalSalesValue = bookCost * booksSold;
+  totalSalesValue = totalSalesValue/2;
+
+  printf("You sold %d books and your take home was $%.2f\n", booksSold, totalSalesValue);
+}
