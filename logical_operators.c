2dd09ef5e282701e10e3b16167c6664f42bba362
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-21 00:34:01 +0000

    Update logical_operators.c

--- logical_operators.c
+++ logical_operators.c
@@ -1,1 +1,12 @@
+int main() {
 
+  int x = 1;
+  int y = 27;
+
+  if (x != y) {
+    printf("Congratulations on setting up a comparison correctly!");
+  } else {
+    printf("Please try again!");
+  }
+
+}
