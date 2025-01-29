ef018f9faba8d9d44303bc37dbf1469a55431068
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-29 09:16:29 +0000

    Update else_clause.c

--- else_clause.c
+++ else_clause.c
@@ -1,1 +1,17 @@
+int main() {
 
+  int grade1 = 59;
+  int grade2 = 90;
+
+  if (grade1 > 60) {
+    printf("Pass\n");
+  } else {
+    printf("Fail\n");
+  }
+
+  if (grade2 > 60) {
+    printf("Pass\n");
+  } else {
+    printf("Fail\n");
+  }
+}
