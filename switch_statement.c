9b2b84583f1673810c42bca0c48944f67d1bb2e2
Paul <118978940+plcr9@users.noreply.github.com>
2025-01-31 06:47:53 +0000

    Update switch_statement.c

--- switch_statement.c
+++ switch_statement.c
@@ -1,1 +1,36 @@
+int main() {
 
+  int number = 7;
+
+  switch(number) {
+    case 1:
+      printf("Bulbasaur\n");
+      break;
+    case 2:
+      printf("Ivysaur\n");
+      break;
+    case 3:
+      printf("Venusaur\n");
+      break;
+    case 4:
+      printf("Charmander\n");
+      break;
+    case 5:
+      printf("Charmeleon\n");
+      break;
+    case 6:
+      printf("Charizard\n");
+      break;
+    case 7:
+      printf("Squirtle\n");
+    case 8:
+      printf("Wartortle\n");
+      break;
+    case 9:
+      printf("Blastoise\n");
+      break;
+    default:
+      printf("Unknown\n");
+      break;
+  }
+}
