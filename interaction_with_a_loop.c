317d6e7ec661e17bd8d9a594a882aa9fdd501055
Paul <118978940+plcr9@users.noreply.github.com>
2025-02-05 07:55:03 +0000

    Update interaction_with_a_loop.c

--- interaction_with_a_loop.c
+++ interaction_with_a_loop.c
@@ -1,1 +1,21 @@
+int main() {
+  int pin = 0;
+  int tries = 0;
 
+  printf("Bank Of Bob:\n");
+  printf("Enter your PIN: ");
+  scanf("%d", &pin);
+
+  tries++;
+
+  while (pin != 1234 && tries < 3) {
+    printf("Enter your PIN: ");
+    scanf("%d", &pin);
+    tries++;
+  }
+
+  if (pin == 1234) {
+    printf("PIN accepted!\n");
+    printf("You have now access.\n");
+  }
+}
