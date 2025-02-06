993454c56a514057fb540e22d03d660b33a4896c
Paul <118978940+plcr9@users.noreply.github.com>
2025-02-06 07:55:47 +0000

    Update guess_the_number.c

--- guess_the_number.c
+++ guess_the_number.c
@@ -1,1 +1,19 @@
+int main() {
 
+  int guess;
+  int tries = 0;
+
+  printf("I'm thinking of a number in the range 1-10.\n");
+  printf("Try to guess it: ");
+  scanf("%d", &guess);
+
+  while (guess != 8 && tries < 50) {
+    printf("Wrong guess, try again: ");
+    scanf("%d", &guess);
+    tries++;
+  }
+
+  if (guess == 8) {
+    printf("You got it!\n");
+  }
+}
