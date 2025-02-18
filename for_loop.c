b740633c559967b08bb7a1a4da80f4daadea9d97
Paul <118978940+plcr9@users.noreply.github.com>
2025-02-18 08:43:41 +0000

    Update for_loop.c

--- for_loop.c
+++ for_loop.c
@@ -1,1 +1,8 @@
+int main() {
 
+  for (int i = 99; i > 0; i--) {
+    printf("%d bottles of pop on the wall.\n", i);
+    printf("Take one down and pass it around.\n");
+    printf("%d bottles of pop on the wall.\n", i - 1);
+  }
+}
