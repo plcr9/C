8e505de9dd915efdab27f484401a1869cd0fbecc
Paul <118978940+plcr9@users.noreply.github.com>
2025-02-25 14:16:56 +0000

    Update array_access_and_element_modification.c

--- array_access_and_element_modification.c
+++ array_access_and_element_modification.c
@@ -1,1 +1,13 @@
+int main() {
 
+  int evens[] = {2, 4, 6, 8, 10, 12};
+  int odds[] = {1, 4, 5, 7, 10, 11};
+
+  printf("%i\n", evens[5]);
+
+  printf("%i\n", evens[2]);
+
+  odds[1] = 3;
+  odds[4] = 9;
+
+}
