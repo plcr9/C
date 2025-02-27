46616bf677d5d1007643e03a1f209f8da109f05d
Paul <118978940+plcr9@users.noreply.github.com>
2025-02-27 00:11:41 +0000

    Update length_of_array.c

--- length_of_array.c
+++ length_of_array.c
@@ -1,1 +1,12 @@
+int main() {
+  double arr[] = {1.0, 8, 2.27, 16.2, 2, 3.14, 12.6, 1.62, 6.54, 9, 11};
+  int len;
 
+  len = sizeof(arr)/sizeof(double);
+
+  printf("%i\n", len);
+
+  for(int i = 0; i < sizeof(arr)/sizeof(double); i++){
+    printf("%f\n", arr[i]);
+  }
+}
