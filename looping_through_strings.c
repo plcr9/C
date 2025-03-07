14f30e32f048480f48d746196180ba3c8fa1c89d
Paul <118978940+plcr9@users.noreply.github.com>
2025-03-07 00:12:59 +0000

    Update looping_through_strings.c

--- looping_through_strings.c
+++ looping_through_strings.c
@@ -1,1 +1,16 @@
+int main() {
+  char s[] = "When the zombies arrive, quickly fax Judge";
+  char p[] = "poolloop";
 
+  for(int i = 0; i < strlen(s); i++){
+    s[i] = "*";
+  }
+
+  printf("%s\n", s);
+
+  for(int i = 0; i < strlen(p)/2; i++){
+    p[i] = '#';
+  }
+
+  printf("%s", p);
+}
