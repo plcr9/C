a4ad50f243a9be49eb6517ecf564e63b3211a38d
Paul <118978940+plcr9@users.noreply.github.com>
2025-03-11 05:25:53 +0000

    Update concatenating_strings.c

--- concatenating_strings.c
+++ concatenating_strings.c
@@ -1,1 +1,19 @@
+int main() {
+  
+  char s1[14] = "London";
+  char s2[] = "Bridge";
 
+  char n[20] = "New";
+  char y[] = "York";
+  char c[] = "City";
+
+  strcat(s1, s2);
+
+  printf("%s\n", s1);
+
+  strcat(n, y);
+  strcat(n, c);
+
+  printf("%s", n);
+
+}
