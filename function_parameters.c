4100242cbadf2d57204aa4d67a2bfccb655f4a17
Paul <118978940+plcr9@users.noreply.github.com>
2025-04-08 13:29:15 +0100

    Update function_parameters.c

--- function_parameters.c
+++ function_parameters.c
@@ -1,1 +1,10 @@
+int getRandonNumber() {
+  int randomNumber = rand() % 1000 + 1;
+  return randomNumber;
+}
 
+int main(void) {
+  srand(time(NULL));
+  int randomNumber = 0;
+  printf("My random number is: %d", randomNumber);
+}
