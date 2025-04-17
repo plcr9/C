d5caca6064ac7b54cd646993853a6c1974b0c133
Paul <118978940+plcr9@users.noreply.github.com>
2025-04-17 07:59:06 +0100

    Update initializing_structures.c

--- initializing_structures.c
+++ initializing_structures.c
@@ -1,1 +1,10 @@
+int main(void) {
+  struct Person {
+    char firstName[25];
+    char lastName[40];
+    int age;
+  };
 
+  struct Person person1 = {"Ada", "Lovelace", 28};
+  struct Person person2 = {"Marie", "Curie", 44};
+}
