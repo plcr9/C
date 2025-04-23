bb202fd7db6e168cf59a009f51fd5fd6ab8b0584
Paul <118978940+plcr9@users.noreply.github.com>
2025-04-23 22:31:56 +0100

    Update why_use_structures.c

--- why_use_structures.c
+++ why_use_structures.c
@@ -1,1 +1,21 @@
+struct Table {
+  int length;
+  int width;
+  int height;
+  char color[20];
+};
 
+int main(void) {
+  int tableLength = 24;
+  int tableWidth = 24;
+  int tableHeight = 20;
+  char tableColor[20] = "Dark Brown";
+
+  int table2Length = 42;
+  int table2Width = 18;
+  int table2Height = 32;
+  char table2Color[20] = "Matte Black";
+
+  struct Table table1 = {24, 24, 20, "Dark Brown"};
+  struct Table table2 = {42, 19. 32, "Matte Black"};
+}
