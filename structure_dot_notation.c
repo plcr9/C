a6a38a821ead1255e26315533e81d103eae84d34
Paul <118978940+plcr9@users.noreply.github.com>
2025-04-24 23:30:49 +0100

    Update structure_dot_notation.c

--- structure_dot_notation.c
+++ structure_dot_notation.c
@@ -1,1 +1,14 @@
+struct Person {
+  char name[40];
+  int age;
+};
 
+int main(void) {
+  struct Person person1 = {"Martin"};
+  struct Person person2 = {"Michelle"};
+
+  person1.age = 57;
+  person2.age = 27;
+  printf("Hello my name is %s and I am %d years old.\n", person1.name, person1.age);
+  printf("Hello my name is %s and I am %d years old.\n", person2.name, person2.age);
+}
