4e35b289c963722bac76917c0c69a3b7407cd792
Paul <118978940+plcr9@users.noreply.github.com>
2025-04-25 06:14:59 +0100

    Update structure_pointers.c

--- structure_pointers.c
+++ structure_pointers.c
@@ -1,1 +1,18 @@
+struct Person {
+  char name[40];
+  int age;
+}
 
+int main(void) {
+  struct Person person1 = {"Martin", 57};
+  struct Person person2 = {"Michelle", 27};
+
+  struct Person* person1Pointer = &person1;
+  struct Person* person2Pointer = &person2;
+
+  (*person1Pointer).age += 1;
+  person2Pointer->age += 10;
+
+  printf("Hello my name is %s and I am %d years old.\n", person1.name, person1.age);
+  printf("Hello my name is %s and I am %d years old.\n", person2.name, person2.age);
+}
